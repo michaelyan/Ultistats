45ea429809ac17b8fc55037e2b2321275bf6aeb4
Michael <michaelyan2@gmail.com>
2012-09-30 11:24:46 -0400

    Added Database functionality

--- src/com/example/ultistats/HomeActivity.java
+++ src/com/example/ultistats/HomeActivity.java
@@ -1,15 +1,42 @@
 package com.example.ultistats;
 
+import java.io.*;
+import com.example.ultistats.model.Player;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        Player p = new Player(this);
+        Cursor cursor = p.listPlayers();
+        
+        String data = "";
+        
+    	if (cursor.moveToFirst()) {
+            do {
+                Log.d(cursor.getString(0), cursor.getString(0));
+                Log.d(cursor.getString(0), cursor.getString(0));
+                data += cursor.getString(1);
+                data += ',';
+                data += cursor.getString(2);
+                data += '\n';
+            } while (cursor.moveToNext());
+        }
+    	
+    	TextView a = new TextView(this);
+    	a.setText(data);
+    	
+//        setContentView(R.layout.activity_home);
+    	setContentView(a);
     }
 
     @Override
